9439cd31658108a65c3f79d2e571b05c968d86e4
T ArunTheja <arun9theja@gmail.com>
2022-06-12 06:18:32 +0000

    Variables_2.sh

---
+++ Shell/Scripts/Variables/Variables_2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "What is your name ?"
+read PERSON                                 # it takes input from above and assigns it as the value of the variable PERSON
+echo "Hello, $PERSON"
