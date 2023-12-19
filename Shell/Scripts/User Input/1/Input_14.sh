82f97f616d95889409582a15dc9a02252f763d5b
aruntheja3 <aruntheja9@hotmail.com>
2023-12-19 04:54:57 +0000

    Input_14.sh

---
+++ Shell/Scripts/User Input/1/Input_14.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+read -p 'username : ' user_var
+read -sp 'password : ' pass_var
+echo "username : $user_var"
+echo "password : $pass_var"
+
+# MultipleLine Comments
+: '
+Here password input doesnot visible while giving in CLI.
+$ ./Input_14.sh
+username : myuser
+password : username : myuser
+password : 12345
+'
