7a2df35d1f57d0bf99f697478cd8dc3fe1a5772b
aruntheja3 <aruntheja9@hotmail.com>
2023-12-19 05:25:07 +0000

    Input_16.sh

---
+++ Shell/Scripts/User Input/1/Input_16.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Enter names : "
+read -a names
+echo "Names : ${names[0]}, ${names[1]}"
+
+# MultipleLine Comments
+: '
+$ ./Input_16.sh
+Enter names : 
+tom max
+Names : tom, max
+'
